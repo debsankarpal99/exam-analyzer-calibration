557966ff74271b2a042906df9502bdca2acbfa16
Debsankar Pal <debsankarpal99@gmail.com>
2025-04-26 16:15:49 +0530

    Update FileUpload.js

--- components/FileUpload.js
+++ components/FileUpload.js
@@ -208,11 +208,19 @@ const FileUpload = ({ onFileProcessed, isProcessing }) => {
         <div className="fixed inset-0 bg-black bg-opacity-70 flex items-center justify-center z-50">
           <div className="text-center p-8 rounded-lg w-full max-w-lg">
             <div className="analyzing-container">
-              <div className="analyzing-text">
-                <span>Analyzing<span className="analyzing-dots"></span></span>
-                <span>Analyzing<span className="analyzing-dots"></span></span>
-                <span>Analyzing<span className="analyzing-dots"></span></span>
+              <div className="analyzing-wave">
+                <span>A</span>
+                <span>N</span>
+                <span>A</span>
+                <span>L</span>
+                <span>Y</span>
+                <span>Z</span>
+                <span>I</span>
+                <span>N</span>
+                <span>G</span>
+                <span className="analyzing-dots"></span>
               </div>
+              <div className="analyzing-pulse"></div>
             </div>
             <p className="text-white text-opacity-80 mt-4">Extracting score data from your exam...</p>
           </div>
